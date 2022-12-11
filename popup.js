b051673d6a6b260df4f7311ccae6d502dbdf7344
IbricAdmir <admiribricbih@gmail.com>
2022-12-11 23:28:15 +0100

    Start gemaakt aan backend, array opgezet, zoekopdrachten worden bekeken naar trefwoorden.

--- popup.js
+++ popup.js
@@ -1,1 +1,25 @@
-console.log("This is a popup!");
+// Array with all the categorys
+let categorieen = [
+    {
+        title: "Scrollen",
+        html: "test.html",
+        keywords: ["scrollen", "scroll"]
+    },
+  ];
+
+  // Get references to the search input and button
+  var searchInput = document.getElementById('search-input');
+  var searchButton = document.getElementById('search-button');
+
+  // Add an event listener to the search button
+  searchButton.addEventListener('click', function() {
+    // Get the search query from the input field
+    var query = searchInput.value;
+
+// Check the keywords property of the object in the categorieen array for the search query
+var keywordsMatch = categorieen[0].keywords.includes(query);
+
+// Log the result of the keywords check to the console
+console.log(keywordsMatch);
+
+  });
