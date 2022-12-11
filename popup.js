91c4888fb96540e5057e40da04713f465c51f49b
IbricAdmir <admiribricbih@gmail.com>
2022-12-11 23:36:48 +0100

    (Officiele Push) Vorige was een foutje
    Start gemaakt aan backend, array opgezet, zoekopdrachten worden bekeken naar trefwoorden.

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-// Array with all the categorys
+// Array with all the category's
 let categorieen = [
     {
         title: "Scrollen",
@@ -7,19 +7,18 @@ let categorieen = [
     },
   ];
 
-  // Get references to the search input and button
-  var searchInput = document.getElementById('search-input');
-  var searchButton = document.getElementById('search-button');
+// Get references to the search input and button
+let searchInput = document.getElementById('search-input');
+let searchButton = document.getElementById('search-button');
 
-  // Add an event listener to the search button
-  searchButton.addEventListener('click', function() {
+// Add an event listener to the search button
+searchButton.addEventListener('click', function() {
     // Get the search query from the input field
-    var query = searchInput.value;
+    let query = searchInput.value;
 
-// Check the keywords property of the object in the categorieen array for the search query
-var keywordsMatch = categorieen[0].keywords.includes(query);
+    // Check the keywords property of the object in the categorieen array for the search query
+    let keywordsMatch = categorieen[0].keywords.includes(query);
 
-// Log the result of the keywords check to the console
-console.log(keywordsMatch);
-
-  });
+    // Log the result of the keywords check to the console
+    console.log(keywordsMatch);
+});
